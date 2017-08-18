93f7e66ec7e8ea7a042e6b0d22e17f0137a35947
Billy Chang <billy.y.chang@gmail.com>
2017-08-18 10:50:35 +1000

    modified it to work with current latest build env

--- app/src/main/java/org/codepond/daggersample/App.java
+++ app/src/main/java/org/codepond/daggersample/App.java
@@ -22,11 +22,13 @@ import android.app.Application;
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
 
 
 public class App extends Application implements HasActivityInjector {
-    @Inject AndroidInjector<Activity> androidInjector;
+
+    @Inject DispatchingAndroidInjector<Activity> androidInjector;
 
     @Override
     public void onCreate() {
